ab04b15b8dd8ddbc528f297527e9896c25e61e50
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-06-27 15:34:30 +0530

    more commits

---
+++ in28minutes-demo/src/main/java/com/in28minutes/springboot/in28minutesdemo/In28minutesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.in28minutesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minutesDemoApplication.class, args);
+	}
+}
---
+++ in28minutes-demo/src/test/java/com/in28minutes/springboot/in28minutesdemo/In28minutesDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.in28minutesdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class In28minutesDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
