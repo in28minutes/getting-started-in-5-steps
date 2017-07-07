9adc04ecc92fbb2f32030c4c771a594b4451cf0c
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-07-07 14:55:34 +0530

    step21

--- jpa-in-10-steps/example.sh
+++ jpa-in-10-steps/example.sh
@@ -1,3 +1,3 @@
-va -cp /ProgrammingExcellence/Workspaces/Rithus.com/ListDirectoryContentInGitFormat/bin test.ListDirectoryContentInGitFormat >> $1.md 
+java -cp /ProgrammingExcellence/Workspaces/Rithus.com/ListDirectoryContentInGitFormat/bin test.ListDirectoryContentInGitFormat >> $1.md 
 git add *; git commit -m "$1"; git push;
 zip -r $1.zip . -x "target/*" -x ".*/*" -x ".*" -x "*.md" -x "mvn*"
