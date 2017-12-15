0aece433a2f22b74ec15530365477d40d2da52c8
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-12-15 11:22:33 +0530

    I'm too lazy to put in a comment

--- jpa-in-10-steps/take-step-backup.sh
+++ jpa-in-10-steps/take-step-backup.sh
@@ -1,2 +1,5 @@
+echo '<!---'  >> $1.md
+echo Current Directory : $PWD >> $1.md
+echo '-->' >> $1.md
 java -cp /ProgrammingExcellence/Workspaces/Rithus.com/ListDirectoryContentInGitFormat/bin test.ListDirectoryContentInGitFormat $PWD >> $1.md
 zip -r $1.zip . -x "target/*"  -x ".mvn/*" -x ".settings/*" -x ".*" -x ".*/*" -x "**/.*" -x "*.md" -x "**/*.md" -x "**/mvn*" -x "*.zip" -x "**/*.zip" -x "/**/target/*" -x "/**/.mvn/*" -x "/**/.settings/*"
