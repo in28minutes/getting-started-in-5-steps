4633ac19cdd464358981eacc6839290f178281a2
bsmahi <bs.mahi@gmail.com>
2023-03-21 09:59:04 +0530

    Upgrade to Spring Boot 3 Latest

--- mockito-in-5-steps/src/main/java/com/in28minutes/mockito/mockitodemo/SomeBusinessImpl.java
+++ mockito-in-5-steps/src/main/java/com/in28minutes/mockito/mockitodemo/SomeBusinessImpl.java
@@ -1,22 +1,23 @@
 package com.in28minutes.mockito.mockitodemo;
 
 public class SomeBusinessImpl {
-	private DataService dataService;
+    private DataService dataService;
 
-	public SomeBusinessImpl(DataService dataService) {
-		super();
-		this.dataService = dataService;
-	}
+    public SomeBusinessImpl(DataService dataService) {
+        super();
+        this.dataService = dataService;
+    }
 
-	int findTheGreatestFromAllData() {
-		int[] data = dataService.retrieveAllData();
-		int greatest = Integer.MIN_VALUE;
+    int findTheGreatestFromAllData() {
+        int[] data = dataService.retrieveAllData();
+        int greatest = Integer.MIN_VALUE;
 
-		for (int value : data) {
-			if (value > greatest) {
-				greatest = value;
-			}
-		}
-		return greatest;
-	}
+        for (int value : data) {
+            if (value > greatest) {
+                greatest = value;
+            }
+        }
+
+        return greatest;
+    }
 }
--- mockito-in-5-steps/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessStubTest.java
+++ mockito-in-5-steps/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessStubTest.java
@@ -1,23 +1,23 @@
 package com.in28minutes.mockito.mockitodemo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class SomeBusinessStubTest {
-	@Test
-	public void testFindTheGreatestFromAllData() {
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
-		int result = businessImpl.findTheGreatestFromAllData();
-		assertEquals(24, result);
+    @Test
+    public void testFindTheGreatestFromAllData() {
+        var businessImpl = new SomeBusinessImpl(new DataServiceStub());
+        int result = businessImpl.findTheGreatestFromAllData();
+        assertEquals(24, result);
 
-	}
+    }
 
 }
 
 class DataServiceStub implements DataService {
-	@Override
-	public int[] retrieveAllData() {
-		return new int[] { 24, 6, 15 };
-	}
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{24, 6, 15};
+    }
 }
